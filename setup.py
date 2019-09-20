f75a7535ea9a646e5a2a50bbdc9134abcb941bc7
ruobin.lb <ruobin.lb@alibaba-inc.com>
2019-09-20 13:24:23 +0800

    upgrade thing access sdk to v0.3.0

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ import sys
 if not (sys.version_info[0] == 3 and sys.version_info[1] == 5 and sys.version_info[2] == 2):
     sys.exit("Link IoT Edge only support Python 3.5.2")
 
-setup(  
+setup(
     name = "lethingaccesssdk",
-    version = "1.0",
+    version = "1.2",
     description = "Link IoT Edge Thing Access SDK for Function Compute",
     license = "Apache 2.0",
   
     url = "https://help.aliyun.com/product/69083.html?spm=a2c4g.11186623.6.540.66bd71b80FSb2h",
-    packages = ['lethingaccesssdk'],
+    packages = ['lethingaccesssdk', 'leda_python'],
     include_package_data = True,
     platforms = "any",
     install_requires = [
